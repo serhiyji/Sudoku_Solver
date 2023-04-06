4691607a1e8f2467258c856ecbba2082dca309a0
serhiyji <serhiy22823@gmail.com>
2023-04-06 12:44:48 +0300

    Add BetterMatrix

---
+++ SudokuSloverHendler/BetterMatrix/BetterMatrix.cs
@@ -0,0 +1,15 @@
+﻿using SudokuSloverHendler.Matrix;
+using SudokuSloverHendler.Points;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSloverHendler.BetterMatrix
+{
+    public partial class BetterMatrix : Matrix<Point>
+    {
+
+    }
+}
