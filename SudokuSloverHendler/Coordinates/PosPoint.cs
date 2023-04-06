7d494c2678b2ecbf5f151404987c985ee266a3c6
serhiyji <serhiy22823@gmail.com>
2023-04-06 12:31:42 +0300

    Add Coordinates

---
+++ SudokuSloverHendler/Coordinates/PosPoint.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSloverHendler.Coordinates
+{
+    public class PosPoint : IComparable<PosPoint>
+    {
+        public int i { get; set; }
+        public int j { get; set; }
+
+        // Ctor
+        #region Ctor
+        public PosPoint(int i_ = 0, int j_ = 0)
+        {
+            i = i_;
+            j = j_;
+        }
+        #endregion
+
+        // operators ==, !=, <, >
+        #region operators ==, !=, <, >
+        public static bool operator ==(PosPoint pos1, PosPoint pos2)
+        {
+            return pos1.Equals(pos2);
+        }
+        public static bool operator !=(PosPoint pos1, PosPoint pos2)
+        {
+            return !pos1.Equals(pos2);
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is PosPoint pos && i == pos.i && j == pos.j;
+        }
+        public static bool operator >(PosPoint pos1, PosPoint pos2)
+        {
+            return pos1.i > pos2.i || pos1.i == pos2.i && pos1.j > pos2.j;
+        }
+        public static bool operator <(PosPoint pos1, PosPoint pos2)
+        {
+            return pos1.i < pos2.i || pos1.i == pos2.i && pos1.j < pos2.j;
+        }
+        #endregion
+
+        public int CompareTo(PosPoint other)
+        {
+            return this == other ? 0 : this < other ? -1 : 1;
+        }
+        public override string ToString()
+        {
+            return $"{i} : {j}";
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
