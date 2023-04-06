c2be6c31cec1c6012e50428d615cbe6c9e6193b2
serhiyji <serhiy22823@gmail.com>
2023-04-06 12:40:15 +0300

    Add Points

---
+++ SudokuSloverHendler/Points/Point.cs
@@ -0,0 +1,48 @@
+﻿using SudokuSloverHendler.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSloverHendler.Points
+{
+    public partial class Point
+    {
+        public int value { get; set; }
+        public Set<int> set;
+        public Point() : this(0)
+        {
+
+        }
+        public Point(int v)
+        {
+            value = v;
+            set = new Set<int>();
+        }
+        public static bool operator ==(Point p1, int value)
+        {
+            return p1.Equals(new Point(value));
+        }
+        public static bool operator !=(Point p1, int value)
+        {
+            return !p1.Equals(new Point(value));
+        }
+        public override string ToString()
+        {
+            return $"{value}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) { return false; }
+            if (!(obj is Point)) { return false; }
+            Point other = (Point)obj;
+            return value == other.value;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ SudokuSloverHendler/Points/ViewPoint.cs
@@ -0,0 +1,20 @@
+﻿using SudokuSloverHendler.Collections;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSloverHendler.Points
+{
+    public partial class Point
+    {
+        public List<Color> ColorsSet { get; set; } = Enumerable.Repeat(Color.FromArgb(0, 0, 0, 0), 9).ToList();
+        public Color ColorValue { get; set; } = Color.FromArgb(0, 0, 0, 0);
+        public void ClearColorsSet()
+        {
+            this.ColorsSet = Enumerable.Repeat(Color.FromArgb(0, 0, 0, 0), 9).ToList();
+        }
+    }
+}
